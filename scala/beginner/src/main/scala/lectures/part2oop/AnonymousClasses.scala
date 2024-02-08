2882d4a1e6f2f9ca984a85222143605feec6c830
Fide Omar Islas Duarte <fidev.id@outlook.com>
2024-02-08 09:45:33 -0600

    learn abstract classes in Scala

---
+++ scala/beginner/src/main/scala/lectures/part2oop/AnonymousClasses.scala
@@ -0,0 +1,33 @@
+package com.fidev.training
+package lectures.part2oop
+
+object AnonymousClasses extends App {
+
+  abstract class Animal {
+    def eat: Unit
+  }
+
+  // anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("ahahahahah")
+  }
+  /*
+    equivalent with
+
+    class AnonymousClasses$$anon$1 extends Animal {
+      override def eat: Unit = println("ahahahahah")
+    }
+    val funnyAnimal = new AnonymousClasses$$anon$1
+   */
+
+  println(funnyAnimal.getClass)
+
+  class Person(name: String) {
+    def sayHi: Unit = println(s"Hi my name is $name, how can I help?")
+  }
+
+  val jim = new Person("") {
+    override def sayHi: Unit = println(s"Hi my name is Jim, how can I be of service?")
+  }
+
+}
