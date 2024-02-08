35f8699954706dc747a6be835db8d1bddb806fa4
Fide Omar Islas Duarte <fidev.id@outlook.com>
2024-02-08 08:26:47 -0600

    abstract data type excercises in Scala

---
+++ scala/beginner/src/main/scala/excercises/MyList.scala
@@ -0,0 +1,43 @@
+package com.fidev.training
+package excercises
+
+abstract class MyList {
+
+  /*
+    head = first element of the list
+    tail = remainder of the list
+    isEmpty = is this list empty
+    add(int) = new list with this element added
+    toString = a string representation of the list
+   */
+
+  def head: Int
+  def tail: MyList
+  def isEmpty: Boolean
+  def add(element: Int): MyList
+  def toString: String
+
+}
+
+object Empty extends MyList {
+  def head: Int = throw new NoSuchElementException
+  def tail: MyList = throw new NoSuchElementException
+  def isEmpty: Boolean = true
+  def add(element: Int): MyList = new Cons(element, Empty)
+  override def toString: String = ???
+}
+
+class Cons(h: Int, t: MyList) extends MyList {
+  def head: Int = h
+  def tail: MyList = t
+  def isEmpty: Boolean = false
+  def add(element: Int): MyList = new Cons(element, this)
+  override def toString: String = ???
+}
+
+object ListTest extends App {
+  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  println(list.tail.head)
+  println(list.add(4).head)
+  println(list.isEmpty)
+}
